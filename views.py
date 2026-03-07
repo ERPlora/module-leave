68ca6c5063ee05bd6a5016f7cfcce6473eea38b5
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-03-07 13:18:22 +0100

    feat: convert drawer panels to full-page forms

--- views.py
+++ views.py
@@ -3,6 +3,8 @@ Leave Management Module Views
 """
 from django.core.paginator import Paginator
 from django.db.models import Q, Count
+from django.http import HttpResponse
+from django.urls import reverse
 from django.shortcuts import get_object_or_404, render as django_render
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -111,6 +113,7 @@ def leave_types_list(request):
     }
 
 @login_required
+@htmx_view('leave/pages/leave_type_add.html', 'leave/partials/leave_type_add_content.html')
 def leave_type_add(request):
     hub_id = request.session.get('hub_id')
     if request.method == 'POST':
@@ -124,10 +127,13 @@ def leave_type_add(request):
         obj.is_paid = is_paid
         obj.is_active = is_active
         obj.save()
-        return _render_leave_types_list(request, hub_id)
-    return django_render(request, 'leave/partials/panel_leave_type_add.html', {})
+        response = HttpResponse(status=204)
+        response['HX-Redirect'] = reverse('leave:leave_types_list')
+        return response
+    return {}
 
 @login_required
+@htmx_view('leave/pages/leave_type_edit.html', 'leave/partials/leave_type_edit_content.html')
 def leave_type_edit(request, pk):
     hub_id = request.session.get('hub_id')
     obj = get_object_or_404(LeaveType, pk=pk, hub_id=hub_id, is_deleted=False)
@@ -138,7 +144,7 @@ def leave_type_edit(request, pk):
         obj.is_active = request.POST.get('is_active') == 'on'
         obj.save()
         return _render_leave_types_list(request, hub_id)
-    return django_render(request, 'leave/partials/panel_leave_type_edit.html', {'obj': obj})
+    return {'obj': obj}
 
 @login_required
 @require_POST
